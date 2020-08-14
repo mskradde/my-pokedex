0015e218d27a8ba3acf7f07c1f005bfddec532d7
mskradde <skradde.marco@gmail.com>
2020-08-14 06:23:51 +0200

    Add Loadingscreen

--- src/App.js
+++ src/App.js
@@ -9,40 +9,46 @@ function waitFor(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 function App() {
+  const [loading, setLoading] = React.useState(true);
+
   const [pokemons, setPokemons] = React.useState(null);
   useEffect(() => {
     async function fetchData() {
       await waitFor(2000);
       const newPokemons = await fetchPokemons();
       setPokemons(newPokemons);
+      setLoading(!loading);
     }
     fetchData();
   }, []);
-
-  return (
-    <div className="app">
-      <header className="header">
-        Pokedex <input className="header__input" placeholder="Search" />
-      </header>
-      <main className="colorful-border">
-        <List>
-          {pokemons?.map((pokemon) => (
-            <ListItem key={pokemon.id} href={pokemon.link}>
-              <ListItemIcon
-                src={pokemon.imgSrc}
-                alt={`Picture of ${pokemon.name}`}
-              />
-              <ListItemText
-                primary={pokemon.name}
-                secondary={`# ${pokemon.id}`}
-              />
-            </ListItem>
-          ))}
-        </List>
-      </main>
-      <footer>Footer</footer>
-    </div>
-  );
+  if (loading) {
+    return <h2>Loading</h2>;
+  } else {
+    return (
+      <div className="app">
+        <header className="header">
+          Pokedex <input className="header__input" placeholder="Search" />
+        </header>
+        <main className="colorful-border">
+          <List>
+            {pokemons?.map((pokemon) => (
+              <ListItem key={pokemon.id} href={pokemon.link}>
+                <ListItemIcon
+                  src={pokemon.imgSrc}
+                  alt={`Picture of ${pokemon.name}`}
+                />
+                <ListItemText
+                  primary={pokemon.name}
+                  secondary={`# ${pokemon.id}`}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </main>
+        <footer>Footer</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
