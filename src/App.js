8cb29b6c70b36f9db4838f091f4e78a97cbbdf7b
mskradde <skradde.marco@gmail.com>
2020-08-17 10:26:21 +0200

    Split app in pages

--- src/App.js
+++ src/App.js
@@ -1,54 +1,21 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
-import ListItem from "./components/ListItem";
-import List from "./components/List";
-import ListItemIcon from "./components/ListItemIcon";
-import ListItemText from "./components/ListItemText";
-import { fetchPokemons } from "./components/PokemonAPI";
-function waitFor(time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
-}
-function App() {
-  const [loading, setLoading] = React.useState(true);
+import Pokemons from "./pages/Pokemons";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-  const [pokemons, setPokemons] = React.useState(null);
-  useEffect(() => {
-    async function fetchData() {
-      await waitFor(2000);
-      const newPokemons = await fetchPokemons();
-      setPokemons(newPokemons);
-      setLoading(!loading);
-    }
-    fetchData();
-  }, []);
-  if (loading) {
-    return <h2>Loading</h2>;
-  } else {
-    return (
+function App() {
+  return (
+    <Router>
       <div className="app">
-        <header className="header">
-          Pokedex <input className="header__input" placeholder="Search" />
-        </header>
-        <main className="colorful-border">
-          <List>
-            {pokemons?.map((pokemon) => (
-              <ListItem key={pokemon.id} href={pokemon.link}>
-                <ListItemIcon
-                  src={pokemon.imgSrc}
-                  alt={`Picture of ${pokemon.name}`}
-                />
-                <ListItemText
-                  primary={pokemon.name}
-                  secondary={`# ${pokemon.id}`}
-                />
-              </ListItem>
-            ))}
-          </List>
-        </main>
+        <Switch>
+          <Route path="/pokemons">
+            <Pokemons />
+          </Route>
+        </Switch>
         <footer>Footer</footer>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
